bbddfb2ea13beb2269389177e42cf01cb69cd6f1
hrys121 <c011426930@edu.teu.ac.jp>
2016-07-08 16:37:06 +0900

    use Metro UI

--- NotToDoList/MainWindow.xaml.cs
+++ NotToDoList/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace NotToDoList {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow {
         public MainWindow() {
             InitializeComponent();
         }
