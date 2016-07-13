5b05421d211989f9503defb787e04ae661575500
hrys121 <c011426930@edu.teu.ac.jp>
2016-07-14 07:34:22 +0900

    DataGrid

--- NotToDoList/MainWindow.xaml.cs
+++ NotToDoList/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using Microsoft.Win32;
 
 namespace NotToDoList {
@@ -23,6 +10,7 @@ namespace NotToDoList {
             InitializeComponent();
         }
         private void Show_Click(object sender, RoutedEventArgs e) {
+            //ファイル名の取得
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.FileName = "";
             ofd.DefaultExt = "*.*";
@@ -31,13 +19,13 @@ namespace NotToDoList {
             }
         }
         private void Add_Click(object sender, RoutedEventArgs e) {
-            /// 項目を動的に追加する
+            /// 項目を動的に追加
             listBox.Items.Add(textBox.Text);
             textBox.Text = "";
         }
 
         private void Del_Click(object sender, RoutedEventArgs e) {
-            /// 項目を削除します
+            /// 項目を削除
             // 選択項目がない場合は処理をしない
             if (listBox.SelectedItems.Count == 0)
                 return;
@@ -47,6 +35,10 @@ namespace NotToDoList {
                 listBox.Items.Remove(
                     listBox.SelectedItems[0]
                 );
+
+        }
+
+        private void comboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
 
         }
     }
