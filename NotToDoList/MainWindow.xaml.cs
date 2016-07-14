5a9409c8478a958b4683e1fff5449dbc108dbc2e
hrys121 <c011426930@edu.teu.ac.jp>
2016-07-14 13:52:19 +0900

    一時保存

--- NotToDoList/MainWindow.xaml.cs
+++ NotToDoList/MainWindow.xaml.cs
@@ -1,13 +1,21 @@
 ﻿using System.Windows;
 using Microsoft.Win32;
+using System;
+using System.Windows.Controls;
+using System.Data;
+using System.Windows.Data;
 
 namespace NotToDoList {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        // データテーブル
+        private DataTable m_dt;
+
         public MainWindow() {
             InitializeComponent();
+            
         }
         private void Show_Click(object sender, RoutedEventArgs e) {
             //ファイル名の取得
@@ -18,29 +26,14 @@ namespace NotToDoList {
                 textBox.Text = ofd.FileName;
             }
         }
-        private void Add_Click(object sender, RoutedEventArgs e) {
-            /// 項目を動的に追加
-            listBox.Items.Add(textBox.Text);
-            textBox.Text = "";
-        }
+      
+       
 
-        private void Del_Click(object sender, RoutedEventArgs e) {
-            /// 項目を削除
-            // 選択項目がない場合は処理をしない
-            if (listBox.SelectedItems.Count == 0)
-                return;
-
-            // 選択された項目を削除
-            while (listBox.SelectedItems.Count > 0)
-                listBox.Items.Remove(
-                    listBox.SelectedItems[0]
-                );
-
-        }
+        
 
         private void comboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
 
         }
-        private void 
+        
     }
 }
