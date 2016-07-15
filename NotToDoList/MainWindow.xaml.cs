4fa7f552d05029e8d0201991dc7a41ed1727112f
hrys121 <c011426930@edu.teu.ac.jp>
2016-07-15 13:03:29 +0900

    未完成

--- NotToDoList/MainWindow.xaml.cs
+++ NotToDoList/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Data;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Threading;
 
 namespace NotToDoList {
     public partial class MainWindow : Window {
@@ -18,36 +19,35 @@ namespace NotToDoList {
 
             m_dt.Columns.Add(new DataColumn("State", typeof(string)));
             m_dt.Columns.Add(new DataColumn("Name", typeof(string)));
-            m_dt.Columns.Add(new DataColumn("Time", typeof(string)));
-            m_dt.Columns.Add(new DataColumn("Path", typeof(string)));
+            m_dt.Columns.Add(new DataColumn("Time", typeof(double)));
+            m_dt.Columns.Add(new DataColumn("Process", typeof(string)));
         }
+        /*
         private void Show_Click(object sender, RoutedEventArgs e) {
             //ファイル名の取得
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.FileName = "";
             ofd.DefaultExt = "*.*";
             if (ofd.ShowDialog() == true) {
-                textBox.Text = ofd.FileName;
+               // textBox.Text = ofd.FileName;
             }
         }
+        */
 
         private void OnAddButtonClick(object sender, RoutedEventArgs e) {
 
             DataRow newRowItem;
             newRowItem = m_dt.NewRow();
-            newRowItem["State"] = "未起動";
+ 
+            newRowItem["State"] = "";
             newRowItem["Name"] = textBox1.Text;
-            newRowItem["Time"] = textBox2.Text + "分";
-            newRowItem["Process"] = textBox.Text;
+            newRowItem["Time"] = textBox2.Text;
+            newRowItem["Process"] = textBox3.Text;
 
             m_dt.Rows.Add(newRowItem);
 
             dataGrid.DataContext = m_dt;
-            dataGrid.HeadersVisibility = DataGridHeadersVisibility.Column;
-
-            textBox.Text = "";
-            textBox1.Text = "";
-            textBox2.Text = "";
+            dataGrid.HeadersVisibility = DataGridHeadersVisibility.Column;            
         }
 
         private void OnDelButtonClick(object sender, RoutedEventArgs e) {
@@ -60,8 +60,6 @@ namespace NotToDoList {
         private void ShowProcess(object sender, RoutedEventArgs e) {
             Process[] hProcesses = Process.GetProcesses();
             
-            string stPrompt = string.Empty;
-
             // 取得できたプロセスからプロセス名を取得する
             foreach (Process hProcess in hProcesses) {
                 listBox.Items.Add(hProcess.ProcessName);
@@ -69,9 +67,35 @@ namespace NotToDoList {
             }
         }
 
-        private void processAdd(object sender, RoutedEventArgs e) {
+        
+        private void processGet(object sender, RoutedEventArgs e) {
             ListBoxItem lbi = (ListBoxItem)listBox.SelectedItem;
             textBox3.Text = lbi.Content.ToString();
+        }
+
+        private void taskKill(object sender, RoutedEventArgs e) {
+            int num = int.Parse(textBox2.Text);
+            Stopwatch sw = new Stopwatch();
+
+            MessageBox.Show("計測を開始します。");
+            sw.Start();
+            Thread.Sleep(num * 1000);  
+            sw.Stop();
+            MessageBox.Show("規定時間に達したためアプリケーションを終了します");
+            while (true) {
+                bool flg = false;
+                Process[] processList = Process.GetProcesses();
+                foreach (Process p in processList) {
+                    if (p.ProcessName == textBox3.Text) {
+                        flg = true;
+                        if (p.Responding) {
+                            p.CloseMainWindow();
+                        }
+                    }
+                }
+                break;
+            }
+
         }
 
         
